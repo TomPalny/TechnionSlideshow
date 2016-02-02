ab2295232b5d871e9da2883c62283df824a4242c
tzjpgit <tompalny@gmail.com>
2016-02-02 15:14:32 +0200

    changed timer to ScheduledThreadPoolExecutor

--- app/src/main/java/com/example/tpalny/myapplication/FullscreenSlideshow.java
+++ app/src/main/java/com/example/tpalny/myapplication/FullscreenSlideshow.java
@@ -12,7 +12,10 @@ import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 
 public class FullscreenSlideshow extends AppCompatActivity {
@@ -25,14 +28,18 @@ public class FullscreenSlideshow extends AppCompatActivity {
     private SharedPreferences settings;
     protected static int heightPixels;
     protected static int widthPixels;
-    //private static Timer loadPicsTimer = Select_Folders.loadPicsTimer;
-    private static Timer loadPicsTimer ;
+    private static Timer loadPicsTimer;
     protected static ImageView imageView1;
     protected static ImageView imageView2;
     protected static ImageView imageView3;
     protected static ImageView imageView4;
     protected static int i = 0;
-
+    private final ScheduledExecutorService textScheduler =
+            Executors.newScheduledThreadPool(1);
+    private final ScheduledExecutorService imageScheduler =
+            Executors.newScheduledThreadPool(2);
+    private ScheduledFuture<?> textUpdateHandle;
+    private ScheduledFuture<?> pictureDisplayHandle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +57,7 @@ public class FullscreenSlideshow extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         heightPixels = metrics.heightPixels;
         widthPixels = metrics.widthPixels;
-
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
 
@@ -69,8 +76,17 @@ public class FullscreenSlideshow extends AppCompatActivity {
 
         if (Select_Folders.isSlideShowWithText) {
             new ReadTextFile().execute();
+            Runnable textUpdate = new Runnable() {
+                @Override
+                public void run() {
+                    new SearchTask(FullscreenSlideshow.this, false, true).execute();
+                    new ReadTextFile().execute();
+                }
+            };
+            Integer rate = Integer.parseInt(Select_Folders.textFileRefreshRate.getText().toString());
+            textUpdateHandle = textScheduler.scheduleAtFixedRate(textUpdate, rate, rate, TimeUnit.MINUTES);
 
-            TimerTask textUpdateTask = new TimerTask() {
+            /*TimerTask textUpdateTask = new TimerTask() {
                 @Override
                 public void run() {
                     new SearchTask(FullscreenSlideshow.this, false, true).execute();
@@ -79,7 +95,7 @@ public class FullscreenSlideshow extends AppCompatActivity {
             };
             textUpdateTimer = new Timer();
             Integer rate = Integer.parseInt(Select_Folders.textFileRefreshRate.getText().toString());
-            textUpdateTimer.schedule(textUpdateTask, rate * 60000, rate * 60000);
+            textUpdateTimer.schedule(textUpdateTask, rate * 60000, rate * 60000);*/
         }
         startTimer();
 
@@ -89,8 +105,16 @@ public class FullscreenSlideshow extends AppCompatActivity {
     public void startTimer() {
 
         Integer delay = Integer.parseInt(Select_Folders.slideShowDelay.getText().toString());
-        //mViewFlipper.setFlipInterval(delay * 1000);
-        final TimerTask loadPicsTask = new TimerTask() {
+        Runnable imageDisplay = new Runnable() {
+            @Override
+            public void run() {
+                new DisplayImage(FullscreenSlideshow.this)
+                        .executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
+            }
+        };
+
+        pictureDisplayHandle = imageScheduler.scheduleAtFixedRate(imageDisplay, 0, delay, TimeUnit.SECONDS);
+        /*final TimerTask loadPicsTask = new TimerTask() {
             @Override
             public void run() {
                 if(loadPicsTimer == null){
@@ -102,18 +126,17 @@ public class FullscreenSlideshow extends AppCompatActivity {
         };
         loadPicsTimer = new Timer();
 
-        loadPicsTimer.schedule(loadPicsTask, 0, delay * 1000);
-        /*new DisplayImage(FullscreenSlideshow.this)
-                .execute();*/
+        loadPicsTimer.schedule(loadPicsTask, 0, delay * 1000);*/
+
     }
 
 
     public void onImageClicked(View view) {
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean(Select_Folders.USER_CANCELLED_SLIDESHOW, true).apply();
-        //mViewFlipper.stopFlipping();
-        cancelTimer(textUpdateTimer);
-        cancelTimer(loadPicsTimer);
+        cancelTimer();
+        /*cancelTimer(textUpdateTimer);
+        cancelTimer(loadPicsTimer);*/
         clearMem();
         finish();
     }
@@ -129,26 +152,29 @@ public class FullscreenSlideshow extends AppCompatActivity {
     public void onBackPressed() {
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean(Select_Folders.USER_CANCELLED_SLIDESHOW, true).apply();
-        //mViewFlipper.stopFlipping();
-        cancelTimer(textUpdateTimer);
-        cancelTimer(loadPicsTimer);
+        cancelTimer();
+        /*cancelTimer(textUpdateTimer);
+        cancelTimer(loadPicsTimer);*/
         clearMem();
         super.onBackPressed();
     }
 
-    private void cancelTimer(Timer timer) {
-        if (timer != null){
+    private void cancelTimer() {
+        textUpdateHandle.cancel(true);
+        pictureDisplayHandle.cancel(true);
+        /*if (timer != null){
             timer.cancel();
             timer.purge();
             timer = null;
-        }
+        }*/
     }
 
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        cancelTimer();
+        clearMem();
         unbindDrawables(findViewById(R.id.root_view));
         System.gc();
     }
